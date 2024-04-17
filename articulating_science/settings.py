8498150a7662b3b7ac7027e0eb3cfe7d2f5e978b
Gwepura <aketani32@gmail.com>
2024-04-17 20:41:36 +0200

    Connect to db

--- articulating_science/settings.py
+++ articulating_science/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'articulating_science.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'artsy_dev',
+        'USER': 'gwepura',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
