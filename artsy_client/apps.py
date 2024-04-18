247519ebb5402f759b8d989edce44b014376578d
Gwepura <aketani32@gmail.com>
2024-04-18 23:08:34 +0200

    Add ArtSy Client

---
+++ artsy_client/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ArtsyClientConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'artsy_client'
---
+++ artsy_client/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+# class Response(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.PROTECT)
+#     session_id = models.CharField(max_length=200)
+#     response = models.CharField(max_length=500)
+#     created_at = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return self.response
