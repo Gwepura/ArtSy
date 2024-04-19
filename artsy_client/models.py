739a2ecab82b4262b0a3d1b3fe8adb122eee4ca3
Gwepura <aketani32@gmail.com>
2024-04-19 10:08:01 +0200

    Remove commented out code in artsy_client/models.py

--- artsy_client/models.py
+++ artsy_client/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
 # Create your models here.
-# class Response(models.Model):
-#     question = models.ForeignKey(Question, on_delete=models.PROTECT)
-#     session_id = models.CharField(max_length=200)
-#     response = models.CharField(max_length=500)
-#     created_at = models.DateTimeField(auto_now_add=True)
-
-#     def __str__(self):
-#         return self.response
